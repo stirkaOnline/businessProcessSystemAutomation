57e47f1165a58927c7a7195e60d94e7e1f780fa8
EugenyTR <takeseoeasy@gmail.com>
2024-12-19 14:36:15 +0700

    feat prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
-import { prisma } from "@/shared/lib/db";
-import { Button } from "@/shared/ui/button";
 import { Card, CardTitle } from "../shared/ui/card";
+import React from "react";
 
 export default async function Home() {
-  const user = await prisma.user.findMany();
-
-  console.log(user);
 
   return (
     <div>
-      <Button size="lg">Hello</Button>
-
-      {user.map((user) => {
-        return (
-          <Card key={user.id}>
-            <CardTitle>{user.username}</CardTitle>
-          </Card>
-        );
-      })}
+      <Card>
+        <CardTitle>User</CardTitle>
+      </Card>
     </div>
   );
 }
